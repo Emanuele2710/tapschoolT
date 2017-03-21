3e1045baff42f69adf99491486f9be4517cc3920
Emanuele2710 <manutose98@yahoo.it>
2017-03-21 22:30:26 +0100

    Add files via upload

---
+++ loginProf/src/loginprof/LoginProf.java
@@ -0,0 +1,30 @@
+
+package loginprof;
+import java.io.*;
+
+/**
+ *
+ * @author Fabio
+ */
+public class LoginProf {
+
+    String username="";
+    String password="";
+    String s=vett.split(";");
+    
+     public void login(String usernameProf,String password)
+        {
+            System.out.println("inserisci il tuo username");
+            
+            System.out.println("inserisci La tua password");
+        }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+    
+}
